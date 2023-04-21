663ed259c6fc0287249614408f9da97c7e0fe208
akkichaturvedi <akki027services@gmail.com>
2023-04-21 21:42:55 +0530

    data commit

--- frontend/src/components/Sidebar/Sidebar.tsx
+++ frontend/src/components/Sidebar/Sidebar.tsx
@@ -1,7 +1,9 @@
+
 import React, { useState } from 'react'
-import { Link } from 'react-router-dom'
-import { useNavigate } from 'react-router-dom'
+import { Link, useNavigate } from 'react-router-dom'
 import Dropdown from './Dropdown/Dropdown'
+
+
 
 function Sidebar({ visibility }: ISideProp) {
     const [dropdownToggle, setDropdownToggle] = useState<boolean>(false)
@@ -97,7 +99,7 @@ function Sidebar({ visibility }: ISideProp) {
                                 </a>
                             </li>
                             <li>
-                                <a href="#" className="relative flex flex-row items-center h-11 focus:outline-none hover:bg-gray-50 text-gray-600 hover:text-gray-800 border-l-4 border-transparent hover:border-indigo-500 pr-6">
+                                <Link to="/faq" className="relative flex flex-row items-center h-11 focus:outline-none hover:bg-gray-50 text-gray-600 hover:text-gray-800 border-l-4 border-transparent hover:border-indigo-500 pr-6">
                                     <span className="inline-flex justify-center items-center ml-4">
                                         <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 172 172" className="w-5 h-5 fill-current dark:text-gray-400">
                                             <path d="M21.5,21.5v129h64.5v-32.25v-64.5v-32.25zM86,53.75c0,17.7805 14.4695,32.25 32.25,32.25c17.7805,0 32.25,-14.4695 32.25,-32.25c0,-17.7805 -14.4695,-32.25 -32.25,-32.25c-17.7805,0 -32.25,14.4695 -32.25,32.25zM118.25,86c-17.7805,0 -32.25,14.4695 -32.25,32.25c0,17.7805 14.4695,32.25 32.25,32.25c17.7805,0 32.25,-14.4695 32.25,-32.25c0,-17.7805 -14.4695,-32.25 -32.25,-32.25z"></path>
@@ -108,11 +110,11 @@ function Sidebar({ visibility }: ISideProp) {
                                             setDropdownToggle(!dropdownToggle)
                                         }}
                                     >partener with us</span>
-                                </a>
+                                </Link>
                             </li>
                             {dropdownToggle ? <Dropdown /> : ""}
                             <li>
-                                <Link to="/faq" className="relative flex flex-row items-center h-11 focus:outline-none hover:bg-gray-50 text-gray-600 hover:text-gray-800 border-l-4 border-transparent hover:border-indigo-500 pr-6">
+                                <Link href="#" className="relative flex flex-row items-center h-11 focus:outline-none hover:bg-gray-50 text-gray-600 hover:text-gray-800 border-l-4 border-transparent hover:border-indigo-500 pr-6">
                                     <span className="inline-flex justify-center items-center ml-4">
                                         <svg className="w-5 h-5" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M7 8h10M7 12h4m1 8l-4-4H5a2 2 0 01-2-2V6a2 2 0 012-2h14a2 2 0 012 2v8a2 2 0 01-2 2h-3l-4 4z"></path></svg>
                                     </span>
