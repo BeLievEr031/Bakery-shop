eefc1f678be54c2491163982bf21429e55aa496f
akkichaturvedi <akki027services@gmail.com>
2023-04-21 21:34:02 +0530

    data added

--- frontend/src/components/Sidebar/Sidebar.tsx
+++ frontend/src/components/Sidebar/Sidebar.tsx
@@ -32,7 +32,7 @@ function Sidebar({ visibility }: ISideProp) {
                                         <svg className="w-5 h-5" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M3 12l2-2m0 0l7-7 7 7M5 10v10a1 1 0 001 1h3m10-11l2 2m-2-2v10a1 1 0 01-1 1h-3m-6 0a1 1 0 001-1v-4a1 1 0 011-1h2a1 1 0 011 1v4a1 1 0 001 1m-6 0h6"></path></svg>
                                     </span>
                                     <span className="ml-2 text-sm tracking-wide truncate">Home</span>
-                                </a>
+                                </Link>
                             </li>
                             <li>
                                 <Link to="/wishlist" className="relative flex flex-row items-center h-11 focus:outline-none hover:bg-gray-50 text-gray-600 hover:text-gray-800 border-l-4 border-transparent hover:border-indigo-500 pr-6">
